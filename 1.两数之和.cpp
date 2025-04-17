05a561576f55816ea88f88292dc7cec25e16ed1b
Tanya_PC <Tanya_PC_2734775341@qq.com>
2025-04-17 21:56:26 +0800

    update

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -6,10 +6,21 @@
 
 // @lc code=start
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
-    }
-};
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) 
+        {
+            for (int i = 0;i < nums.size();i++)
+            {
+                for (int j = i + 1;j < nums.size();j++)
+                {
+                    if (nums.at(i) + nums.at(j) == target)
+                    {
+                        return { i,j };
+                    }
+                }
+            }
+            return {};
+        }
+    };
 // @lc code=end
 
