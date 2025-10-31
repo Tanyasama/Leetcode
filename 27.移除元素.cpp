420d72ba95a1d7033be4af635e708f5553f1cbf2
Tanya <2734775341@qq.com>
2025-10-31 16:38:19 +0800

    代码随想录

---
+++ 27.移除元素.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=cpp
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int i = 0;
+
+        while(i<n)
+        {
+            if(nums[i]==val)
+            {
+                nums[i] = nums[n-1];
+                n--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return n;
+    }
+};
+// @lc code=end
+
