008d9300eee2667710c7b0030fe9af5b299821a2
Tanya <2734775341@qq.com>
2025-11-05 16:40:34 +0800

    代码随想录

---
+++ 59.螺旋矩阵-ii.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=59 lang=cpp
+ *
+ * [59] 螺旋矩阵 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n,vector<int>(n,0));
+        int num = 1;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+        while(left <= right && top <= bottom)
+        {
+            for(int j=left;j<=right;j++)
+            {
+                matrix[top][j]=num++;
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++)
+            {
+                matrix[i][right]=num++;
+            }
+            right--;
+
+            if(top<=bottom)
+            {
+                for(int j = right;j >= left;j--)
+                {
+                    matrix[bottom][j]=num++;
+                }
+                bottom--;
+            }
+
+            for(int i = bottom;i>=top;i--)
+            {
+                matrix[i][left]=num++;
+            }
+            left++;
+        }
+        return matrix;
+    }
+};
+// @lc code=end
+
